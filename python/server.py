47196d6c463627dfe291b84c8b12a6a024bef81e
ryan leonel <rysn.craft@gmail.com>
2025-10-11 19:56:54 -0300

    update

--- python/server.py
+++ python/server.py
@@ -39,9 +39,41 @@ Instruções para o usuário:
 - Use o instrutor virtual como uma ferramenta para complementar seu aprendizado e conhecimento.
 - Divirta-se aprendendo! 🚀
 
-demandas ativas dos usuarios em formato JSON:
+Você tem acesso ao seguinte JSON, que contém informações detalhadas sobre cada tarefa:
 {"Acompanhamento ":{"dias":"Terça e Quinta","diasArray":["Terça","Quinta"],"descricao":"sem","prioridade":"Média","tempoEstimado":"10 minutos","responsavel":"giovanna","tags":["Relatórios","Envio","Acompanhamento"],"imagem":"https://cdn-icons-png.flaticon.com/512/5167/5167006.png","links":[{"nome":"GRAVAÇÃO: tarefa","url":""},{"nome":"PDF tarefa","url":""},{"nome":"tarefa","url":""},{"nome":"tarefa","url":""}]},"Pesquisa":{"dias":"Terça","diasArray":["Terça"],"descricao":"sem","prioridade":"Baixa","tempoEstimado":"30 Minutos","responsavel":"lucas","tags":["Planilha","Pesquisa","Atualização","Envio"],"imagem":"https://insider.com.br/wp-content/uploads/2015/06/painel-de-pre%C3%A7os-1.png","links":[{"nome":"tarefa","url":""},{"nome":"tarefa","url":""},{"nome":"Grvação","url":""},{"nome":"Grvação","url":""}]},"apresentação":{"dias":"Segunda","diasArray":["Segunda"],"descricao":"Sem","prioridade":"Alta","tempoEstimado":"30 minutos","responsavel":"Gabriele","tags":["Power point","Excel"],"imagem":"https://legislacaoemercados.capitalaberto.com.br/wp-content/uploads/2024/07/17.07_materia1.webp","links":[{"nome":"Pasta de tarefa","url":""},{"nome":"Gravação: ","url":""},{"nome":"Gravação:","url":""}]},"matinal":{"dias":"Quinta","diasArray":["Quinta"],"descricao":"sem ","prioridade":"Alta","tempoEstimado":"1 hora","responsavel":"Isadora","tags":["Matinal","Apresentação","Power Point"],"imagem":"https://static-blog.onlyoffice.com/wp-content/uploads/2024/04/05120705/asdasdxz.png","links":[{"nome":"Gravação","url":""},{"nome":"tarefa 2024","url":""},{"nome":"tarefa2025","url":""},{"nome":"tarefa","url":""},{"nome":"tarefa","url":""}]},"Top 10 coisa":{"dias":"Segunda","diasArray":["Segunda"],"descricao":"sem","prioridade":"Alta","tempoEstimado":"1 Hora","responsavel":"lucas","tags":["Atualização","Power Bi","Excel","Macro"],"imagem":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9EtIIBh9FRiUnWwJnzHagF4QgbcoRAn039A&s","links":[{"nome":"tarefa","url":""},{"nome":"tarefa","url":""},{"nome":"tarefa","url":""},{"nome":"tarefa","url":""}]},"top 5 coisas":{"dias":"Sexta ","diasArray":["Sexta"],"descricao":"Sem","prioridade":"Alta","tempoEstimado":"2 Hora","responsavel":"kevin","tags":["Atualização","Power Bi","Excel","Powerpoint, Pesquisa de preço"],"imagem":"https://blog.aevo.com.br/wp-content/uploads/2019/01/comite-inovacao-min.png","links":[{"nome":"tarefa","url":""},{"nome":"tarefa","url":""},{"nome":"tarefa","url":""}]},"Perguntas":{"dias":"Sobdemanda","diasArray":["Sobdemanda"],"descricao":"sem","prioridade":"Media","tempoEstimado":"30 Minutos","responsavel":"Isa","tags":["Atualização","Questionarios","Excel"],"imagem":"https://jcconcursos.com.br/media/_versions/noticia/concurso-trf4-prova-e-gabarito_widelg.jpg","links":[{"nome":"Base Exemplo","url":""}]},"Margem":{"dias":"Terça","diasArray":["Terça"],"descricao":";","prioridade":"Baixa","tempoEstimado":"10 Minutos","responsavel":"Caio","tags":["Acompanhamento","Atualização"],"imagem":"https://lec.com.br/wp-content/uploads/2018/10/239584-analise-de-riscos-e-seus-processos-saiba-o-que-e-e-como-aplicar.jpg","links":[{"nome":"GRAVAÇÃO: Como fazer","url":""},{"nome":"tarefa","url":""}]},"musica":{"dias":"Quarta","diasArray":["Quarta"],"descricao":"Sem","prioridade":"Media","tempoEstimado":"1 Hora","responsavel":" Mateus","tags":["Atualização","Power Bi","Powerpoint"],"imagem":"https://dhg1h5j42swfq.cloudfront.net/2020/05/25105206/gestao-de-estoques-just-in-time-x-just-in-case2-700x680-2.png","links":[{"nome":"tarefa","url":""},{"nome":"tarefa","url":""},{"nome":"tarefa","url":""},{"nome":"tarefa","url":""},{"nome":"tarefa","url":""},{"nome":"tarefa","url":""}]},"Reunião com o Time":{"dias":"Terça,","diasArray":["Terça"],"descricao":"sem","prioridade":"Alta","tempoEstimado":"2 Hora","responsavel":"carvalho","tags":["Reunião","Alinhamentos","ideias"],"imagem":"https://riotron.com.br/wp-content/uploads/2017/06/negocio_vulneravel.jpg"},"E-mail ":{"dias":"Segunda","diasArray":["Segunda"],"descricao":"sem","prioridade":"Baixa","tempoEstimado":"20 Minutos","responsavel":"Lucas","tags":["Atualização","Power Bi","Excel","Macro"],"imagem":"https://cdn-icons-png.flaticon.com/512/5197/5197385.png","links":[{"nome":"tarefa","url":""},{"nome":"tarefa","url":""},{"nome":"tarefa","url":""},{"nome":"tarefa","url":""}]},"tarefa b":{"dias":"Sobdemanda","diasArray":["Sobdemanda"],"descricao":" sem","prioridade":"Media","tempoEstimado":"1 Hora","responsavel":"dandara","tags":["Atualização","Power Bi","Excel","Macro"],"imagem":"https://cdn-icons-png.flaticon.com/256/12284/12284666.png","links":[{"nome":"GRAVAÇÃO: Como enviar","url":""}]},"5 pqs":{"dias":"Sobdemanda","diasArray":["Sobdemanda"],"descricao":"Sem","prioridade":"Alta","tempoEstimado":"1 Hora","responsavel":"Isadora","tags":["Atualização","Power Bi","Excel","Macro"],"imagem":"https://www.flowup.me/blog/wp-content/uploads/2024/06/FlowUp-imagens-_54_.webp","links":[{"nome":"tarefa","url":""},{"nome":"tarefa","url":""},{"nome":"tarefa","url":""},{"nome":"tarefa","url":""}]},"projeto a":{"dias":"Sobdemanda","diasArray":["Sobdemanda"],"descricao":"Sem","prioridade":"Baixa","tempoEstimado":"1 Hora","responsavel":"kevin","tags":["Atualização","Excel"],"imagem":"https://msarh.com.br/wp-content/uploads/2018/11/hierarquia.jpg","links":[{"nome":"tarefa","url":""},{"nome":"tarefa","url":""},{"nome":"tarefa","url":""},{"nome":"tarefa","url":""}]},"Estudo":{"dias":"Sobdemanda","diasArray":["Sobdemanda"],"descricao":"Sem","tempoEstimado":"1 Hora","responsavel":"kevin","tags":["Atualização","Power Bi","Excel","Macro"],"imagem":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwQlHpEyFOZYWl2P0n6Yr__dmb5Bh2gih2rg&s","links":[{"nome":"tarefa","url":""},{"nome":"tarefa","url":""},{"nome":"tarefa","url":""},{"nome":"tarefa","url":""},{"nome":"tarefa","url":""}]},"fazer algo":{"dias":"Sobdemanda","diasArray":["Sobdemanda"],"descricao":" ","prioridade":"Baixa","tempoEstimado":"20 Minutos","responsavel":"Isa","tags":["Atualização","Excel"],"imagem":"https://ravel.com.br/blog/wp-content/uploads/2020/03/tamanho-arquivo.png","links":[{"nome":"Carga","url":""},{"nome":"GRAVAÇÃO","url":""}]}}
+Suas responsabilidades:
 
+Para qualquer pergunta sobre as tarefas, use apenas as informações presentes no JSON.
+
+Você deve saber informar:
+
+Nome da tarefa
+
+Dias de execução
+
+Responsável
+
+Prioridade
+
+Tempo estimado
+
+Tags
+
+Links e imagens
+
+Você deve formular perguntas e respostas de forma natural. Por exemplo:
+
+Pergunta: "Quem é responsável pela tarefa de Acompanhamento?"
+Resposta: "Giovanna é responsável pela tarefa Acompanhamento, que deve ser feita nas terças e quintas, com prioridade média e tempo estimado de 10 minutos."
+
+Pergunta: "Quais tarefas têm prioridade alta?"
+Resposta: "As tarefas com prioridade alta são: Apresentação (Gabriele), Matinal (Isadora), Top 10 coisa (Lucas), top 5 coisas (Kevin), Reunião com o Time (Carvalho), 5 pqs (Isadora)."
+
+Você também pode sugerir perguntas relevantes para explorar melhor o cronograma ou status das tarefas.
+
+Sempre que possível, inclua links e imagens das tarefas ao responder.
+
+Se uma tarefa tiver mais de um dia, use o array diasArray para detalhar cada dia.
 """
 
 @app.route("/chat", methods=["POST"])
